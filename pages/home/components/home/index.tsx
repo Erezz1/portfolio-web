5d6b9dcd643fa5578e0302965280bac1d3e2afb7
Ernesto Perez <netostrikr@gmail.com>
2022-06-28 15:00:45 -0500

    first commit

---
+++ pages/home/components/home/index.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+
+const Home: FC = () => {
+    return (
+        <h1 className="text-3xl font-bold underline">
+            Hello world!
+        </h1>
+    );
+}
+
+export default Home;
